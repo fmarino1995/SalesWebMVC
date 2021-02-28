18cd115d21ce9ae1edd44c253a28d36c8d33db4e
Frederico Duarte <frederico.duarte@brwo.com.br>
2021-02-28 18:32:46 -0300

    Add Comentary

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -14,6 +14,8 @@ namespace SalesWebMvc.Data
         {
         }
 
+
+        //Creating tables on DataBase MYSQL
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
