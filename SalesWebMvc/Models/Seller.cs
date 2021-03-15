13c2875a25f8f59254d0905186a23bc44c756295
Frederico Duarte <fmarinoduarte@gmail.com>
2021-03-15 19:13:01 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -10,8 +10,13 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        //{0} = Name , {1} = Primeiro atributo , {2} = Segundo atributo 
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
@@ -20,8 +25,10 @@ namespace SalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
@@ -61,3 +68,6 @@ namespace SalesWebMvc.Models
         }
     }
 }
+
+
+
